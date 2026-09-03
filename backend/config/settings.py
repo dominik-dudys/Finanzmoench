8b340a465d6197faf0992243259a5b588103c3a7
Julian Eichhorn <julian.eichhorn17@gmail.com>
2026-09-03 13:34:31 +0200

    Nginx Implementation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ry9j+=208ih642u@kz432$c5y_x1+k(f2w%=_^(h&@^^)^d*#%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Für Produktion später echte Domain eintragen!!
 
 
 # Application definition
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
