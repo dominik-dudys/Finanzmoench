84c80daf478dc1b803e011b7b03383618aed0df9
dominik-dudys <dominik.dudys@icloud.com>
2026-09-14 09:03:01 +0200

    Tried to fix Docker problem

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,14 @@ import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "@/shared/api/query-client.ts";
 import {RouterProvider} from "react-router";
 import {router} from "@/app/router.tsx";
+import {AuthProvider} from "@/features/auth";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router}/>
+          <AuthProvider>
+              <RouterProvider router={router}/>
+          </AuthProvider>
       </QueryClientProvider>
   </StrictMode>,
 )
