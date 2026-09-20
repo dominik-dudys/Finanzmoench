111ce4fcd6cb698a6ff3ee61b77582d0c7041bfc
Julian Eichhorn <julian.eichhorn17@gmail.com>
2026-09-20 21:22:52 +0200

    update households/urls.py

--- backend/households/urls.py
+++ backend/households/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path('create/', CreateHouseholdView.as_view(), name='household-create'),
     path('join/', JoinHouseholdView.as_view(), name='household-join'),
-    path('me/', MyHouseholdsView.as_view(), name='my-households'),
+    path('myhouseholds'/', MyHouseholdsView.as_view(), name='my-households'),
     path('update/', UpdateHouseholdView.as_view(), name='update-household'),
     path('leave/', LeaveHouseholdView.as_view(), name='leave-household'),
     path('delete/', DeleteHouseholdView.as_view(), name='delete-household'),
